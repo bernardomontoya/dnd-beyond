cc6fd4e8ba8088cacc3fb62b1c32de537b99438e
Bernardo Montoya <ber.montoya12@gmail.com>
2024-09-23 16:47:56 -0600

    feat: add Roboto Mono font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,13 @@
+import { Roboto_Mono } from "next/font/google";
 import type { Metadata } from "next";
 
 import { StyledComponentsRegistry } from "@/lib/registry";
+import "./globals.css";
+
+const robotoMono = Roboto_Mono({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={robotoMono.className}>
       <body>
         <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
       </body>
