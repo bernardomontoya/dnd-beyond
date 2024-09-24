53744a924e44e5d76dad74a9f08e0e3e600c7963
Bernardo Montoya <ber.montoya12@gmail.com>
2024-09-24 00:57:24 -0600

    feat: highlight tracker when points are spent

--- src/components/Tracker/Tracker.tsx
+++ src/components/Tracker/Tracker.tsx
@@ -8,10 +8,18 @@ type TrackerProps = {
 };
 
 export const Tracker = ({ current, label, total, error }: TrackerProps) => {
+  const isTrackerComplete = current === total;
+
   return (
     <div className={styles.tracker}>
       <span className={styles.points}>{`${current} / ${total}`}</span>
-      <span className={styles.label}>{label}</span>
+      <span
+        className={`${styles.label} ${
+          isTrackerComplete ? styles.labelComplete : ""
+        }`}
+      >
+        {label}
+      </span>
       {error && <span className={styles.error}>{error}</span>}
     </div>
   );
